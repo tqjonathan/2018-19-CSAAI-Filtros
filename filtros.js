b6bce8ecbca8f4bddb7afa4676e54eb9ae702cd4
tqjonathan <ja.toctaguano@alumnos.urjc.es>
2019-05-08 13:14:00 +0200

    commit

--- filtros.js
+++ filtros.js
@@ -1,90 +1,78 @@
 function main() {
-  console.log("Empezamos");
+  console.log("Empezamos")
 
-  var img = document.getElementById('img_original');
+  //-- Imagen original
+  var img = document.getElementById('imagesrc')
+  //-- Canvas
   var canvas = document.getElementById('display');
 
-  var display_R = document.getElementById('display_R')
-  var deslizador_R = document.getElementById('deslizador_R');
-  var deslizador_G = document.getElementById('deslizador_G');
-  var deslizador_B = document.getElementById('deslizador_B');
+  //-- RGB
+  Rojo = document.getElementById('Rojo')
+  Verde = document.getElementById('Verde')
+  Azul = document.getElementById('Azul')
+  //-- Valores RGB
+  R_value = document.getElementById('R_value');
+  G_value = document.getElementById('G_value');
+  B_value = document.getElementById('B_value');
 
-
-  // IDEA: establece el tamaño del canvas = que la imagen a preocesar
+  // Tamaño canvas = imagen
   canvas.width = img.width;
   canvas.height = img.height;
-  // IDEA: Contexto del canvas para trabajar
-  var ctx = canvas.getContext("2d");
-  // IDEA: dibuja la imagen original en el canvas
-  ctx.drawImage(img, 0, 0);
 
-/////////// FILTRO R ////////////////////////
-  deslizador_R.oninput = () => {
-    display_R.innerHTML = deslizador_R.value
-    ctx.drawImage(img, 0,0);
-    //Obtener la imagen del canvas en pixeles
-    // En Chrome este comando da error, utilizar Firefox
-    // para funcionar en chrome añadir al directorio de chrome:
-    //--allow-file-access-from-files
-    var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    //-- Obtener el array con todos los píxeles
-    var data = imgData.data
-    //-- Obtener el umbral de rojo del desliador
-    umbral_R = deslizador_R.value
-    //-- Filtrar la imagen según el nuevo umbral
+  // Contexto canvas
+  var ctx = canvas.getContext("2d");
+
+  //Colocar imagen en canvas (no modificado)
+  ctx.drawImage(img, 0,0);
+  // Obtener la imagen del canvas en pixeles
+  var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+  // Umbrales RGB
+  function print_img(data){
+    Umbral_R = Rojo.value
+    Umbral_V = Verde.value
+    Umbral_A = Azul.value
     for (var i = 0; i < data.length; i+=4) {
-      if (data[i] > umbral_R)
-        data[i] = umbral_R;
+      if (data[i] > Umbral_R){
+        data[i] = Umbral_R;
+      }
+      if (data[i+1] > Umbral_V){
+        data[i+1] = Umbral_V;
+      }
+      if (data[i+1] > Umbral_A){
+        data[i+2] = Umbral_A;
+      }
     }
-      //-- Poner la imagen modificada en el canvas
-    ctx.putImageData(imgData, 0, 0);
   }
 
+  //
+  Rojo.oninput = () => {
+    //-- Mostrar el nuevo valor del Rojo
+    R_value.innerHTML = Rojo.value
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-///////////////////// DATOS DE LA IMAGEN /////////////////////////////
-/*
-  console.log("Tamaño de data: " + data.length);
-  npixels = canvas.width * canvas.height
-  console.log("Anchura (en pixeles): " + canvas.width)
-  console.log("Altura (en pixeles): " + canvas.height)
-  console.log("Pixeles totales: " + npixels)
-  //-- Puesto que cada pixel ocupa 4 bytes, el array de píxeles
-  //-- tiene un tamaño de 4 * numero de pixeles
-  console.log("Total de datos de la imagen: " + npixels * 4)
-////////////////////////////////////////////////////////////////////
-*/
+    ctx.drawImage(img, 0,0);
+    var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    
+    //-- Obtener el array con todos los píxeles
+    var data = imgData.data
+    print_img(data);
+    //-- Poner la imagen modificada en el canvas
+    ctx.putImageData(imgData, 0, 0);
+  }
+  Verde.oninput = () => {
+    G_value.innerHTML = Verde.value
+    ctx.drawImage(img, 0,0);
+    var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    var data = imgData.data
+    print_img(data)
+    ctx.putImageData(imgData, 0, 0);
+  }
+  Azul.oninput = () => {
+    B_value.innerHTML = Azul.value
+    ctx.drawImage(img, 0,0);
+    var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    var data = imgData.data
+    print_img(data)
+    ctx.putImageData(imgData, 0, 0);
+  }
 }
